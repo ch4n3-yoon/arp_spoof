b8cec0b3525b8769a10e71a1bca1438757a98f12
chaneyoon <chaneyoon@gmail.com>
2017-08-04 03:00:57 +0900

    add ARP Operation code

--- arp_spoof.py
+++ arp_spoof.py
@@ -8,7 +8,7 @@ import threading
 import signal
 
 from scapy.all import *
-from scpay.base_classes import Gen, Set Gen
+from scpay.base_classes import Gen, SetGen
 import scapy.plist as plist
 from scapy.utils import *	# PcapReader
 from scapy.data import *	# MTU, ETH_P_ARP
@@ -27,10 +27,30 @@ arp_spoof <interface> <sender ip 1> <target ip 1> [<sender ip 2> <target ip 2>..
 
 """
 
+
+# ARP Operation Code
+ARP_REQUEST		= 0x1
+ARP_REPLY		= 0x2
+RARP_REQUEST 	= 0x3
+RARP_REPLY		= 0x4
+
+
+
 def get_mac_address(ifname):
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
 	return ':'.join(['%02x' % ord(char) for char in info[18:24]])
+
+
+
+def get_mac(ip_addr):
+	"get_mac is used to obtain the mac address of the target ip."
+	print "Getting Mac for: %s" % ip_addr
+		responses, unanswered = srp(Ether(dst="de:ad:be:ef:ca:fe")/ARP(pdst=ip_addr),timeout=2,retry=10)
+	for s,r in responses:
+		return r[Ether].src
+	return None
+
 
 
 # function to check user name
